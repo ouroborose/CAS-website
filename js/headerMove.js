d6f244fcedcc83fc45247d41085eaccfcf4bb0d2
ouroborose <RosePeng@Roses-MacBook-Pro.local>
2015-09-05 22:11:12 -0500

    Beginnings
    Modified version of “agency” template, built on Bootstrap.

---
+++ js/headerMove.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        $("#box").animate({height: "300px"});
+    });
+    $("#btn2").click(function(){
+        $("#box").animate({height: "100px"});
+    });
+});
