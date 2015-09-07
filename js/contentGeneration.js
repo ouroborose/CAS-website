210dd6af60c1a7d00860e9e5892f3c3260969bb7
Vu Ha <stregawolf@gmail.com>
2015-09-06 23:03:48 -0700

    json test 9

--- js/contentGeneration.js
+++ js/contentGeneration.js
@@ -13,18 +13,21 @@ var GeneratePortfolioItem = function(data)
 
 	if(data.hasOwnProperty("id"))
 	{
+		console.log("setting id");
 		var modalIdHolder = portfolioItem.find("#portfolio-item-modal-id");
 		modalIdHolder.href = "#" + data["id"] + "-modal";
 	}
 
 	if(data.hasOwnProperty("portfolio-item-title"))
 	{
+		console.log("setting title");
 		var title = portfolioItem.find("#portfolio-item-title");
 		title.innerHTML = data["portfolio-item-title"];
 	}
 	
 	if(data.hasOwnProperty("portfolio-item-image"))
 	{
+		console.log("setting image");
 		var image = portfolioItem.find("#portfolio-item-image");
 		image.src = data["portfolio-item-image"];
 	}
