3268967c5be06ac8fc702de5291aed2ee7fef0b7
Vu Ha <stregawolf@gmail.com>
2015-09-06 22:23:13 -0700

    Test loading from json 1

---
+++ js/contentGeneration.js
@@ -0,0 +1,23 @@
+var games = [
+    "SuperSeaSerpentSimulator",
+];
+
+
+var GenerateFromJason = function(data)
+{
+	console.log(data);
+};
+
+var GenerateContent = function(game)
+{
+	$.getJSON("data/" + game + ".json", GenerateFromJason);
+	//console.log("Loading " + game);
+    //$("#modals").append($("<div>").load("modals/" + game + ".html"));
+};
+
+$(document).ready(function(){
+	for (var i = games.length - 1; i >= 0; i--) {
+		var game = games[i];
+		GenerateContent(game);
+	}
+});
